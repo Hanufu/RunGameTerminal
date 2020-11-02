f18a96a76db62039e6c6c9926f532c2b6b6a1c91
Eduardo Ribeiro da Silva <45810581+Hanufu@users.noreply.github.com>
2020-11-02 20:01:45 -0300

    Ajustes na criação do mapa e posição do usuario
    Adcionei jogador, ajustei como o mapa era mostrado e criado, refatorei o codigo e criei o loop inicial do game

--- Run.c
+++ Run.c
@@ -3,10 +3,6 @@
 #include <string.h>
 #include <locale.h>
 
-//definindo tamanho do mapa
-#define LARGURA_M 14
-#define ALTURA_M 7
-
 struct player{
 	char nome[50];
 	char user;
@@ -32,32 +28,58 @@ int main(){
 	
 	//----------Campo das Variaveis-----------
 	typedef struct player Player;
-	int linha,coluna;
+	int i,j ;
 	Player jogador;
 	jogador.x =0;
 	jogador.y =0;
 	jogador.user = '@';
+	char tecla;
 	//----------Fim Campo Variaveis------------
+	char mapa[7][14];
 	
-	
-	//criadno mapa
-	char mapa[LARGURA_M][ALTURA_M];
-	for(linha =0; linha <= ALTURA_M; linha++){
-		for(coluna =0; coluna <= LARGURA_M; coluna++){
-			mapa[linha][coluna] ='-';
+	//loop do game
+	while(tecla != 'q'){
+		
+		menuGame();
+		
+		//criando mapa
+		for(i = 0; i<7; i++){
+			for(j = 0; j<14; j++){
+				if(jogador.x == i && jogador.y==j){
+					mapa[i][j] = jogador.user;
+				}else{
+					mapa[i][j] = '-';
+				}
+				
+			}
 		}
+		//mostrando mapa
+		for(i = 0; i<7; i++){
+			for(j = 0; j<14; j++){
+				printf("%c", mapa[i][j]);
+			}
+			printf("\n");
+		}
+		
+		//capturando tecla do usuario para mover o personagem
+		tecla = getch();
+		
+		if(tecla == 'd' || tecla == 'D'){
+			jogador.y++;
+		}else if(tecla == 'a' || tecla == 'A'){
+			jogador.y--;
+		}else if(tecla == 's' || tecla == 'S'){
+			jogador.x++;
+		}else if(tecla == 'w' || tecla == 'W'){
+			jogador.x--;
+		}
+		
+		system("cls");// limpa a tela para fazer animação do personagem se movendo
 	}
 	
-	for(linha =0; linha <= ALTURA_M; linha++){
-		for(coluna =0; coluna <= LARGURA_M; coluna++){
-			printf("%c", mapa[linha][coluna]);
-		}
-		printf("\n");
-	}
 	
 	
 	
-	
-	
+
 	return 0;
 }
